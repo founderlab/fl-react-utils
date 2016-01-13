f45dff4d9a700d5f5313f9fc4cb409b63eca7396
gwilym <gwilym@gwilymh.com>
2016-01-14 10:18:47 +1100

    always_fetch option for serverRenderer

--- readme.md
+++ readme.md
@@ -1,7 +1,25 @@
 # Boilerplate and helper functions for react/redux that can be shared amongst FounderLab apps
 
+### Components
+
+#####createServerRenderer:
+Helper method that takes care of a bunch of bs boilerplate for rendering react components server side. 
+Usage: 
+
+```javascript
+app.get('*', createServerRenderer({
+  createStore, 
+  getRoutes,
+  scripts: _.map(_.pick(require('../../webpack-assets.json'), ['shared.js', 'app']), entry => entry.js),
+  omit: 'admin',
+  always_fetch: require('../../shared/modules/app/containers/App'),
+  config: _.pick(config, config.client_config_keys),
+}))
+```
+
 Changes: 
 
+- 0.3.3: always_fetch option for serverRenderer: List components here that should have their fetchData method called regardless of the route
 - 0.3.2: createGroupByReducer added
 - 0.3.0: Naming scheme updated; pagination added
 - 0.2.0: dispatchNeeds changed to fetchComponentData
--- src/createServerRenderer.js
+++ src/createServerRenderer.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import Queue from 'queue-async'
 import React from 'react'
 import {renderToString} from 'react-dom/server'
 import createHistory from 'history/lib/createMemoryHistory'
@@ -8,68 +9,83 @@ import {reduxReactRouter, match} from 'redux-router/server'
 
 import fetchComponentData from './fetchComponentData'
 
+const sendError = (res, err) => {
+  console.log(err)
+  return res.status(500).send('Error loading initial state')
+}
+
 export default function createServerRenderer(options) {
   const {createStore, getRoutes, scripts=[], config={}} = options
+  let always_fetch = options.always_fetch || []
+  if (!_.isArray(always_fetch)) always_fetch = [always_fetch]
   if (!createStore) throw new Error('[fl-react-utils] createServerRenderer: Missing createStore from options')
   if (!getRoutes) throw new Error('[fl-react-utils] createServerRenderer: Missing getRoutes from options')
 
   return function app(req, res) {
+    const queue = new Queue(1)
+
     const server_state = {
       config,
       auth: req.user ? {user: _.omit(req.user.toJSON(), 'password')} : {},
     }
-    const store = createStore(reduxReactRouter, getRoutes, createHistory, server_state)
+    if (options.loadInitialState) {
+      queue.defer(callback => options.loadInitialState((err, state) => {
+        if (err) return sendError(res, err)
+        callback(null, _.merge(server_state, state))
+      }))
+    }
+    queue.await(err => {
+      console.log('server_state', server_state)
+      if (err) return sendError(res, err)
 
-    store.dispatch(match(req.originalUrl, (err, redirect_location, router_state) => {
-      if (err) {
-        console.log(err)
-        return res.status(500).send('Internal server error')
-      }
-      if (redirect_location) return res.redirect(redirect_location.pathname + redirect_location.search)
-      if (!router_state) return res.status(404).send('Not found')
+      const store = createStore(reduxReactRouter, getRoutes, createHistory, server_state)
 
-      fetchComponentData({store, components: router_state.components}, (err) => {
-        if (err) {
-          console.log(err)
-          return res.status(500).send('Internal server error')
-        }
+      store.dispatch(match(req.originalUrl, (err, redirect_location, router_state) => {
+        if (err) return sendError(res, err)
+        if (redirect_location) return res.redirect(redirect_location.pathname + redirect_location.search)
+        if (!router_state) return res.status(404).send('Not found')
 
-        let initial_state = store.getState()
+        const components = _.uniq((always_fetch || {}).concat(router_state.components))
+        fetchComponentData({store, components}, (err) => {
+          if (err) return sendError(res, err)
 
-        // temp solution to rendering admin state
-        // todo: make this better. don't include admin reducers / route unless requested
-        if (options.omit) initial_state = _.omit(initial_state, options.omit)
+          let initial_state = store.getState()
 
-        // https://github.com/rackt/redux-router/issues/106
-        router_state.location.query = req.query
+          // temp solution to rendering admin state
+          // todo: make this better. don't include admin reducers / route unless requested
+          if (options.omit) initial_state = _.omit(initial_state, options.omit)
 
-        const component = (
-          <Provider store={store} key="provider">
-            <ReduxRouter />
-          </Provider>
-        )
+          // https://github.com/rackt/redux-router/issues/106
+          router_state.location.query = req.query
 
-        const script_tags = scripts.map(script => `<script type="application/javascript" src="${script}"></script>`).join('\n')
+          const component = (
+            <Provider store={store} key="provider">
+              <ReduxRouter />
+            </Provider>
+          )
 
-        const HTML = `
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <meta charset="utf-8">
-              <title>FounderLab_replaceme></title>
-              <script type="application/javascript">
-                window.__INITIAL_STATE__ = ${JSON.stringify(initial_state)}
-              </script>
-            </head>
-            <body id="app">
-              <div id="react-view">${renderToString(component)}</div>
-              ${script_tags}
-            </body>
-          </html>
-        `
-        res.type('html').send(HTML)
+          const script_tags = scripts.map(script => `<script type="application/javascript" src="${script}"></script>`).join('\n')
 
-      })
-    }))
+          const HTML = `
+            <!DOCTYPE html>
+            <html>
+              <head>
+                <meta charset="utf-8">
+                <title>FounderLab_replaceme></title>
+                <script type="application/javascript">
+                  window.__INITIAL_STATE__ = ${JSON.stringify(initial_state)}
+                </script>
+              </head>
+              <body id="app">
+                <div id="react-view">${renderToString(component)}</div>
+                ${script_tags}
+              </body>
+            </html>
+          `
+          res.type('html').send(HTML)
+
+        })
+      }))
+    })
   }
 }
