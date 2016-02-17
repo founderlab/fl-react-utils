1e7e0b9a6d2ff9d8eef0b511e1fe432b79bc2536
gwilym <gwilym@gwilymh.com>
2016-02-17 14:52:31 +1100

    pagination buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,6 @@
 import _ from 'lodash' // eslint-disable-line
 import React, {Component, PropTypes} from 'react'
-import {ButtonToolbar, ButtonGroup, Button} from 'react-bootstrap'
+import {ButtonToolbar, ButtonGroup, Button, Glyphicon} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 
 export default class Pagination extends Component {
@@ -9,18 +9,45 @@ export default class Pagination extends Component {
     items_per_page: PropTypes.number.isRequired,
     current_page: PropTypes.number.isRequired,
     total_items: PropTypes.number.isRequired,
+    max_links: PropTypes.number,
+  }
+
+  static defaultProps = {
+    max_links: 4,
   }
 
   render() {
     const {location, items_per_page, current_page, total_items} = this.props
+    const max_links = this.props.max_links - 1
     const links = []
     const total_pages = Math.ceil(total_items / items_per_page)
     if (!total_pages) return null
 
-    for (let i=1; i<=total_pages; i++) {
+    let start = Math.min(Math.floor(current_page - max_links/2), total_pages-max_links)
+    if (start < 1) start = 1
+    const end = Math.min(start+max_links, total_pages)
+
+    if (start > 1) {
+      links.push(
+        <LinkContainer key={1} to={location.pathname} query={_.extend({}, location.query, {page: 1})}>
+          <Button bsStyle="default">1</Button>
+        </LinkContainer>
+      )
+      start = start + 1
+    }
+
+    for (let i=start; i<=end; i++) {
       links.push(
         <LinkContainer key={i} to={location.pathname} query={_.extend({}, location.query, {page: i})}>
           <Button bsStyle={current_page === i ? 'primary' : 'default'}>{i}</Button>
+        </LinkContainer>
+      )
+    }
+
+    if (end < total_pages) {
+      links.push(
+        <LinkContainer key="next" to={location.pathname} query={_.extend({}, location.query, {page: current_page+1})}>
+          <Button bsStyle="default"><Glyphicon glyph="chevron-right" /></Button>
         </LinkContainer>
       )
     }
