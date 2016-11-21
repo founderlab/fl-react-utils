6108131282d748c5cfa2f0cb71a227ef2363460f
gwilym <gwilym@gwilymh.com>
2016-11-21 15:10:03 +1100

    Form feedback only applies when the input is not active

--- src/validation.js
+++ src/validation.js
@@ -2,20 +2,21 @@ import moment from 'moment'
 
 // Validation highlighting for react-bootstrap Input component
 export function validationState(field) {
-  if (!field || !field.touched) return null
+  if (!field || !field.touched || field.active) return null
   return field.error ? 'error' : 'success'
 }
 
 // Validation highlighting for other input component
 export function validationStyle(field) {
-  if (!field || !field.touched) return null
+  if (!field || !field.touched || field.active) return null
   return field.error ? 'has-error' : 'has-success'
 }
 
 // Vadliation help text for react-bootstrap Input component
 export function validationHelp(field) {
   if (!field) return null
-  return (field.touched && field.error) || null
+  field.touched && field.error && console.log('help field', field)
+  return (field.touched && !field.active && field.error) || null
 }
 
 export function validDate(current) {
