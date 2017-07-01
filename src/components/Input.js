61d5b388bd08d62d3f84a22055f8d61f66c9d5a6
gwilym <gwilym@gwilymh.com>
2017-07-01 11:33:56 +1000

    Accept list of strings as options for select inputs

--- src/components/Input.js
+++ src/components/Input.js
@@ -133,9 +133,12 @@ export default class Input extends React.Component {
         control = (
           <FormControl componentClass="select" {...inputProps} value={inputProps.value}>
             {this.props.includeEmpty && (<option />)}
-            {_.map(options, option => (
-              <option key={option.value} value={option.value}>{option.label}</option>
-            ))}
+            {_.map(options, opt => {
+              const option = _.isObject(opt) ? opt : {label: opt, value: opt}
+              return (
+                <option key={option.value} value={option.value}>{option.label}</option>
+              )
+            })}
           </FormControl>
         )
         break
